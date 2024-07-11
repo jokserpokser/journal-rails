97a7507346ab2000da7ad1864741bd84957e4838
Eljan Peralta <eljan233@gmail.com>
2024-07-11 19:21:44 +0800

    Added test for show view

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -10,6 +10,11 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get show" do
+    get category_url(@category)
+    assert_response :success
+  end
+
   test "should get new" do
     get new_category_url
     assert_response :success
